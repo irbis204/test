9f35a545a8c0f2b8a3fe7853f1d4b182c64b34b6
irbis204 <ghoster7725@gmail.com>
2021-05-31 12:12:24 +0300

    0001

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: '/home/john213/PhpstormProjects/untitled/scripts/main.js',
+    output: {
+        filename: 'main.js',
+        path: '/home/john213/PhpstormProjects/untitled/dist',
+    },
+    watch: true,
+}
